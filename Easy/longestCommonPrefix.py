e2b6d0eb4a2265730e52e21c4d5eb71ecbb043dd
Aryan Singh <83677526+aru24sin@users.noreply.github.com>
2024-12-04 16:30:02 -0500

    Update longestCommonPrefix.py

--- Easy/longestCommonPrefix.py
+++ Easy/longestCommonPrefix.py
@@ -13,3 +13,14 @@ class Solution(object):
                 return ans
             ans += first[i]
         return ans
+
+#test input to test function
+ans = Solution().longestCommonPrefix(["flower", "flow", "flight"])
+print(ans)
+
+"""
+This is a perfect simple example of using two pointers I believe. I have seen this pattern before
+Where you traverse through the list in a similar elementary method as determining the median from 
+a list of numbers in a list. There was no need for using a dictionary or nested loops as I 
+believed.
+"""
